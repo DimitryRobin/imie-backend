b2c51b246e3d19933ebdd4279b0fe5f3d19166e7
equinveros <e.quinveros@gmail.com>
2017-10-05 11:40:19 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -10,12 +10,13 @@ var hbs = require('hbs');
 var log4js = require('log4js');
 var logger = log4js.getLogger();
 log4js.configure({
-	appenders: { console : {type: 'console'} },
-	categories: { default: { appenders: ['console'], level: 'info' } }
+    appenders: {console: {type: 'console'}},
+    categories: {default: {appenders: ['console'], level: 'info'}}
 });
 
 var index = require('./routes/index');
 var weather = require('./routes/weather');
+var meteo = require('./routes/meteo');
 
 var app = express();
 app.use(helmet());
@@ -30,29 +31,30 @@ app.set('view engine', 'hbs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(morgan('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/weather', weather);
+app.use('/meteo', meteo);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
