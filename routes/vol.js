2883cff7a45f91a923b9ac2c871ece82b08cf3f5
equinveros <e.quinveros@gmail.com>
2017-10-05 16:32:53 +0200

    Add files via upload

---
+++ routes/vol.js
@@ -0,0 +1,67 @@
+var express = require('express');
+var router = express.Router();
+var log4js = require('log4js');
+var logger = log4js.getLogger();
+var http = require('http');
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/imie_vol', function (err) {
+    if (err) {
+        throw err;
+    }
+});
+var Schema = mongoose.Schema;
+var volSchema = new Schema({
+    name: String,
+    author: String,
+    date: {type: Date, default: Date.now}
+});
+
+var volModel = mongoose.model('Vol', volSchema);
+var datas = [];
+
+router.get('/', function (req, res, next) {
+    var query = volModel.find(null)
+    query.exec(function (err, vols) {
+        if (err) {
+            throw err;
+        }
+        // On va parcourir le résultat et les afficher joliment
+        var vol;
+        for (var i = 0, l = vols.length; i < l; i++) {
+            vol = vols[i];
+            logger.info('------------------------------');
+            logger.info('Nom : ', vol.name);
+            logger.info('Auteur : ', vol.author);
+            logger.info('Date : ', vol.date);
+            logger.info('ID : ', vol._id);
+            logger.info('------------------------------');
+            datas[i] = ({'nom': vol.name, 'auteur': vol.author, 'date': vol.date})
+            logger.info("Got a response: ", datas);
+        }
+        res.render('vol', {place :'vol',data: datas, json: JSON.stringify(datas)});
+    });
+
+});
+
+router.post('/', function (req, res, next) {
+
+    var newVol = new volModel({name: req.body.name, author: req.body.author});
+    var dateN = Date.now;
+    newVol.save(function (err) {
+        if (err) {
+            throw err;
+        } else {
+            if (req.body.date) {
+                dateN = req.body.date
+            }
+            datas.push({"nom": req.body.name, "auteur" : req.body.author, "date" : dateN});
+        }
+
+        console.log('Vol ajouté avec succès !');
+        res.render('vol',{ place: 'vol' ,data: datas, json: JSON.stringify(datas)});
+    });
+});
+
+
+module.exports = router;
